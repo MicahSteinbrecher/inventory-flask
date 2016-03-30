ada678454da88d82d52b7d83422878aaeebf36c7
Micah <micahsteinbrecher@go.wustl.edu>
2016-03-29 20:19:14 -0400

    more changes

--- app.py
+++ app.py
@@ -1,17 +1,17 @@
 import os
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session, redirect
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.heroku import Heroku
 
 app = Flask(__name__)
 
 #db for local developement
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/sample_db'
-#db = SQLAlchemy(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/sample_db'
+db = SQLAlchemy(app)
 
 #db for cloud
-heroku = Heroku(app)
-db = SQLAlchemy(app)
+#heroku = Heroku(app)
+#db = SQLAlchemy(app)
 
 #Create db model
 class User(db.Model):
@@ -27,14 +27,22 @@ class User(db.Model):
 
 #Set homepage
 @app.route('/')
+@app.route('/index')
+@app.route('/')
 def home():
     """Render website's home page."""
-    return render_template('index.html')
+    if 'username' in session:
+        user = User.query.filter_by(username=session["user_id"]).first()
+        return render_template('index.html',
+                               user = user)
+    else:
+        return render_template('login.html') #LOGIN AND REGISTER PAGE
 
 # Save e-mail to database
-@app.route('/register', methods=['POST'])
-def register():
+@app.route('/create_user', methods=['POST'])
+def create_user():
     email = None
+    error = None
     if request.method == 'POST':
         email = request.form['email']
         #check email does not exist
@@ -42,7 +50,18 @@ def register():
             new_user = User(email)
             db.session.add(new_user)
             db.session.commit()
-            return render_template('index.html')
-        return render_template('index.html')
+            return render_template('index.html',
+                                   user = email)
+        error = 'This e-mail is already assosciated with a user!'
+        return render_template('login.html',
+                               registration_error = error)
+
+@app.route('/validate_login', methods=['POST'])
+def validate_login():
+    email = request.form['email']
+    if db.session.query(User).filter(User.email == email).count():
+        return render_template('index.html',
+                               user = email)
+
 if __name__ == '__main__':
     app.run(debug=True)
